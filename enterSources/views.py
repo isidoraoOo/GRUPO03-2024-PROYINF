0690b9367a72d8e9081348965dd11f55d229dd0a
Rodrigo Ramirez <rodrigo.ramirezd@usm.cl>
2024-10-26 04:42:03 -0300

    segundo avance del proyecto, insercion de fuentes exitosas, falta pulir el frontend y detalles de logica pueden mejorar.

--- enterSources/views.py
+++ enterSources/views.py
@@ -1,31 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.http import HttpResponse
 from enterSources.models import Sources
+from enterSources.forms import Bulletin_request
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Inicio")
-
-def service(request):
     return render(request, "index.html")
 
-def enter_source(request):
-    source = request.GET.get("source", None)
-    sources = []
-
-    if(request.GET["source"]):
-        message = "Fuente a ingresar: %r" %request.GET["source"]
-        source = request.GET["source"]
-
-        sources = Sources.object.filter(__name__icontains=source)
+def service(request):
+    if request.method == 'POST':
+        form1 = Bulletin_request(request.POST)
+        if form1.is_valid():
+            form1.save()
+            messages.success(request, 'Fuente agregada exitosamente.')
+            return redirect('../result_insert/')  # Redirigir a una página de éxito o lista
     else:
-        message = "No se ha introducido fuentes."
+        messages.error(request, 'Hubo un error al agregar la fuente. Por favor, revisa los campos.')
+        form1 = Bulletin_request()
+    
+    return render(request, 'insert.html', {'form': form1})
 
-    return render(request, "result_insert.html", {"sources":sources, "query":source, "message":message})
-
-def home(request):
-    return HttpResponse("Inicio")
-
-def home(request):
-    return HttpResponse("Inicio")
+def result_insert(request):
+    return HttpResponse("Exito al ingresar la fuente de datos.")
